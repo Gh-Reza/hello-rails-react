70f92a0c64c3a747d7c39a6ca3a1d5785a1aa936
Reza Merzaie <ghrezam97@gmail.com>
2023-11-29 00:20:16 +0430

    Fix linters

--- test/controllers/api/v1/random_controller_test.rb
+++ test/controllers/api/v1/random_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::RandomControllerTest < ActionDispatch::IntegrationTest
-  test "should get random_greeting" do
+  test 'should get random_greeting' do
     get api_v1_random_random_greeting_url
     assert_response :success
   end
